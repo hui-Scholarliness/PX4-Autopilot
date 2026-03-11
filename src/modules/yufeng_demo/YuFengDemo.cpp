555bc19e97feb6deaa48fceac69e5f2592185ab9
辉——书生意气 <3233456044@qq.com>
2026-03-11 16:17:27 +0800

    【新增】在qgc中显示变量

---
+++ src/modules/yufeng_demo/YuFengDemo.cpp
@@ -0,0 +1,147 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013-2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "YuFengDemo.hpp"
+
+#include <float.h>
+#include <lib/mathlib/mathlib.h>
+#include <px4_platform_common/events.h>
+
+#include <lib/matrix/matrix/math.hpp>
+
+using namespace matrix;
+
+YuFengDemo::YuFengDemo()
+    : ModuleParams(nullptr),
+      ScheduledWorkItem(MODULE_NAME,
+                        px4::wq_configurations::nav_and_controllers) {
+  // parameters_update(true);
+}
+
+YuFengDemo::~YuFengDemo() { perf_free(_cycle_perf); }
+
+bool YuFengDemo::init() {
+  if (!_local_pos_sub.registerCallback()) {
+    PX4_ERR("callback registration failed");
+    return false;
+  }
+
+  _time_stamp_last_loop = hrt_absolute_time();
+  ScheduleNow();
+
+  return true;
+}
+
+void YuFengDemo::parameters_update(bool force) {}
+
+void YuFengDemo::Run() {
+  if (should_exit()) {
+    _local_pos_sub.unregisterCallback();
+    exit_and_cleanup();
+    return;
+  }
+  // reschedule backup
+  ScheduleDelayed(100_ms);
+
+  parameters_update(false);
+
+  perf_begin(_cycle_perf);
+  vehicle_local_position_s vehicle_local_position;
+
+  if (_local_pos_sub.update(&vehicle_local_position))
+  {
+    const float dt = math::constrain(
+        ((vehicle_local_position.timestamp_sample - _time_stamp_last_loop) *
+         1e-6f),
+        0.002f, 0.04f);
+    _time_stamp_last_loop = vehicle_local_position.timestamp_sample;
+    if(_param_yu_feng_en.get())
+    {
+      printf("hello sky! dt=%.4f\n", (double)dt);
+    }
+    else
+    {
+      printf("hello land!\r\n");
+    }
+
+  }
+  perf_end(_cycle_perf);
+}
+
+int YuFengDemo::task_spawn(int argc, char* argv[]) {
+  YuFengDemo* instance = new YuFengDemo();
+
+  if (instance) {
+    _object.store(instance);
+    _task_id = task_id_is_work_queue;
+
+    if (instance->init()) {
+      return PX4_OK;
+    }
+
+  } else {
+    PX4_ERR("alloc failed");
+  }
+
+  delete instance;
+  _object.store(nullptr);
+  _task_id = -1;
+
+  return PX4_ERROR;
+}
+
+int YuFengDemo::custom_command(int argc, char* argv[]) {
+  return print_usage("unknown command");
+}
+
+int YuFengDemo::print_usage(const char* reason) {
+  if (reason) {
+    PX4_WARN("%s\n", reason);
+  }
+
+  PRINT_MODULE_DESCRIPTION(
+      R"DESCR_STR(
+### Description
+	YuFeng demo.
+)DESCR_STR");
+
+  PRINT_MODULE_USAGE_NAME("yufeng_demo", "controller");
+  PRINT_MODULE_USAGE_COMMAND("start");
+  PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+  return 0;
+}
+
+extern "C" __EXPORT int yufeng_demo_main(int argc, char* argv[]) {
+  return YuFengDemo::main(argc, argv);
+}
---
+++ src/modules/yufeng_demo/yufeng_demo_params.c
@@ -0,0 +1,23 @@
+/**
+ * YuFeng Enable
+ *
+ * display the yufeng hello sky.
+ *
+ * @boolean
+ * @group Demo
+ */
+PARAM_DEFINE_INT32(YU_FENG_EN,0);
+
+/**
+ * YuFeng length
+ *
+ * Yufeng UAV length.
+ *
+ * @unit m
+ * @min 0.0
+ * @max 1.0
+ * @decimal 3
+ * @increment 0.01
+ * @group Demo
+ */
+PARAM_DEFINE_FLOAT(YU_FENG_LEN, 0.225f);
